9309d2a20afe1a213a097d49f54258be8b86209a
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2021-04-18 04:40:28 +0700

    svelte array of strings and array of objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,24 @@
 <script>
   import TodoCard from "./TodoCard.svelte";
+  // let contents = ["Belajar CSS", "Belajar JS", "Belajar ES6", "Belajar Svelte"]; //array of strings
+  let cards = [
+    {
+      todo: "Belajar CSS",
+      list: "task",
+    },
+    {
+      todo: "Belajar JS",
+      list: "task",
+    },
+    {
+      todo: "Belajar ES6",
+      list: "task",
+    },
+    {
+      todo: "Belajar Svelte",
+      list: "task",
+    },
+  ];
 </script>
 
 <div class="container my-3">
@@ -10,10 +29,16 @@
       <div class="card-header">Todo</div>
       <div class="card-body">
         <ul class="list-group">
-          <TodoCard content={"Belajar HTML"} />
-          <TodoCard content={"Belajar CSS"} />
-          <TodoCard content={"Belajar JS"} />
-          <TodoCard content={"Belajar ES6"} />
+          <!-- For array of strings -->
+          <!-- {#each contents as content} -->
+          <!-- <TodoCard content={content} /> -->
+          <!-- <TodoCard {content} /> -->
+          <!-- {/each} -->
+
+          <!-- for array of objects -->
+          {#each cards as card}
+            <TodoCard content={card.todo} />
+          {/each}
         </ul>
       </div>
     </div>
