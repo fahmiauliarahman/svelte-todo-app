6541d525bc511993a5312196b18a1255bab30388
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2021-04-18 04:33:14 +0700

    make FE Card and props

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
 <script>
-	export let name;
+  import TodoCard from "./TodoCard.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container my-3">
+  <h3>Todo App</h3>
+
+  <div class="card-deck">
+    <div class="card bg-light">
+      <div class="card-header">Todo</div>
+      <div class="card-body">
+        <ul class="list-group">
+          <TodoCard content={"Belajar HTML"} />
+          <TodoCard content={"Belajar CSS"} />
+          <TodoCard content={"Belajar JS"} />
+          <TodoCard content={"Belajar ES6"} />
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/TodoCard.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let content;
+</script>
+
+<li class="list-group-item mb-2">{content}</li>
