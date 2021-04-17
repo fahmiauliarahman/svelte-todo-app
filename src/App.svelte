e09abfc445cfdf4faa4b41bb23426474f65e9d2f
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2021-04-18 05:30:58 +0700

    create card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,23 @@
 <script>
-  import TodoCard from "./TodoCard.svelte";
-  let cards = [
+  import CardList from "./CardList.svelte";
+  let todoCards = [
     {
       todo: "Belajar CSS",
       list: "task",
     },
-    {
-      todo: "Belajar JS",
-      list: "task",
-    },
-    {
-      todo: "Belajar ES6",
-      list: "task",
-    },
-    {
-      todo: "Belajar Svelte",
-      list: "task",
-    },
   ];
-  let todo = "";
 
-  function handleAddCard() {
-    cards = [...cards, { todo, list: "task" }];
-    console.log(`${todo} successfully added.`);
-    todo = "";
-  }
+  let doingCards = [];
+  let doneCards = [];
 </script>
 
 <div class="container my-3">
   <h3>Todo App</h3>
 
   <div class="card-deck">
-    <div class="card bg-light">
-      <div class="card-header">Todo</div>
-      <div class="card-body">
-        <ul class="list-group">
-          {#each cards as card}
-            <TodoCard content={card.todo} />
-          {/each}
-          <input
-            type="text"
-            name="todo-input"
-            class="form-control mb-2"
-            id="todo-input"
-            bind:value={todo}
-          />
-          <button on:click={handleAddCard} class="btn btn-success">Add</button>
-        </ul>
-      </div>
-    </div>
+    <CardList cards={todoCards} listName="To Do" />
+    <CardList cards={doingCards} listName="Doing" />
+    <CardList cards={doneCards} listName="Done" />
   </div>
 </div>
 
---
+++ src/CardList.svelte
@@ -0,0 +1,29 @@
+<script>
+  import TodoCard from "./TodoCard.svelte";
+  export let cards, listName;
+  let todo = "";
+  function handleAddCard() {
+    cards = [...cards, { todo, list: "task" }];
+    console.log(`${todo} successfully added.`);
+    todo = "";
+  }
+</script>
+
+<div class="card bg-light">
+  <div class="card-header">{listName}</div>
+  <div class="card-body">
+    <ul class="list-group">
+      {#each cards as card}
+        <TodoCard content={card.todo} />
+      {/each}
+      <input
+        type="text"
+        name="todo-input"
+        class="form-control mb-2"
+        id="todo-input"
+        bind:value={todo}
+      />
+      <button on:click={handleAddCard} class="btn btn-success">Add</button>
+    </ul>
+  </div>
+</div>
